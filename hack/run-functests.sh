879bda73779610870dd347fb1bdee4ea963f241b
Jack Ding <jackding@gmail.com>
2023-04-18 19:53:36 -0400

    Install ginkgo
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- hack/run-functests.sh
+++ hack/run-functests.sh
@@ -2,20 +2,16 @@
 set -x
 which ginkgo
 if [ $? -ne 0 ]; then
-# we are moving to a temp folder as in go.mod we have a dependency that is not
-# resolved if we are not using google's GOPROXY. That is not the case when building as
-# we are using vendored dependencies
-	GINKGO_TMP_DIR=$(mktemp -d)
-	cd $GINKGO_TMP_DIR
-	go mod init tmp
-	go get github.com/onsi/ginkgo/ginkgo@v1.16.5
-	rm -rf $GINKGO_TMP_DIR
-	echo "Downloading ginkgo tool"
-	cd -
+  echo "Downloading ginkgo tool"
+  go install -mod=mod github.com/onsi/ginkgo/ginkgo@v1.16.5
 fi
 
 GOPATH="${GOPATH:-~/go}"
 JUNIT_OUTPUT="${JUNIT_OUTPUT:-/tmp/artifacts/unit_report.xml}"
 export PATH=$PATH:$GOPATH/bin
 
+# silence deprecations
+export ACK_GINKGO_DEPRECATIONS=1.16.5
+# silence Ginkgo 2.0 notice
+export ACK_GINKGO_RC=true
 GOFLAGS=-mod=vendor ginkgo "$SUITE" -- -junit $JUNIT_OUTPUT
