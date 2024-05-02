eb1d5bc6501f9d0a53847feb1c894e27261f98a7
Jack Ding <7378668+jzding@users.noreply.github.com>
2024-05-02 11:36:47 -0400

    [release-4.15] Fix consumer and e2e aws test (#309)
    * Enhance Consumer Application with Periodic Publisher Health Check
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * update dev dockerfiles
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * add PreferredDuringScheduling annotation to consumer
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * Fix consumer image and e2e-aws test
    
    Fix event subscription in consumer example.
    Update consumer image and deployment.
    Remove amq from e2e test.
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    ---------
    
    Signed-off-by: Jack Ding <jackding@gmail.com>

--- hack/deploy_test.sh
+++ hack/deploy_test.sh
@@ -2,53 +2,33 @@
 
 set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source "$DIR/common/common.sh"
-source "$DIR/common/cne.sh"
-source "$DIR/common/consumer.sh"
-source "$DIR/common/router.sh"
-
 KUBECONFIG="${KUBECONFIG:-}"
 CNE_IMG="${CNE_IMG:-quay.io/openshift/origin-cloud-event-proxy}"
 CONSUMER_IMG="${CONSUMER_IMG:-quay.io/redhat-cne/cloud-event-consumer}"
 CONSUMER_TYPE="${CONSUMER_TYPE:-MOCK}"
 
+source "$DIR/common/common.sh"
+source "$DIR/common/cne.sh"
+source "$DIR/common/consumer.sh"
 
 if [ "$KUBECONFIG" == "" ]; then
 	echo "[ERROR]: No KUBECONFIG provided"
 	exit 1
 fi
 action=$1
-ttype=$2
-if [ "$ttype" == "HTTP" ]; then
-  TransportType="HTTP"
-else
- TransportType="AMQ"
-fi
+TransportType="HTTP"
+
 
 if [ "$action" == "undeploy" ]; then
   action="delete"
-  deploy_amq $action $NamespaceAMQTesting || true
   deploy_consumer $action $NamespaceConsumerTesting "" "" || true
   deploy_producer $action $NamespaceProducerTesting "" || true
   create_namespaces $action || true
-elif [ "$action" == "deploy-amq" ]; then
-  action="apply"
-  create_amq_namespaces $action || true
-  deploy_amq $action $NamespaceAMQTesting || true
-elif [ "$action" == "delete-amq" ]; then
-  action="delete"
-  deploy_amq $action $NamespaceAMQTesting || true
-  create_amq_namespaces $action || true
 elif [ "$action" == "deploy-consumer" ]; then
   action="apply"
   create_consumer_namespaces $action || true
-    if [ "$TransportType" == "AMQ" ]; then
-        deploy_consumer $action $NamespaceConsumerTesting $AMQTransportHost "" || true
-        deploy_producer $action $NamespaceProducerTesting $AMQTransportHost || true
-    elfi
-        deploy_consumer $action $NamespaceConsumerTesting $HTTPConsumerTransportHost $HTTPMockTransportHost || true
-        deploy_producer $action $NamespaceProducerTesting $HTTPMockTransportHost || true
-    fi
+  deploy_consumer $action $NamespaceConsumerTesting $HTTPConsumerTransportHost $HTTPMockTransportHost || true
+  deploy_producer $action $NamespaceProducerTesting $HTTPMockTransportHost || true
 elif [ "$action" == "delete-consumer" ]; then
   action="delete"
   deploy_consumer $action $NamespaceConsumerTesting "" "" || true
@@ -57,13 +37,6 @@ else
   action="apply"
   label_node || true
   create_namespaces $action || true
-  # currently ginkgo cannot skip amq test, so leave it for now
-  deploy_amq $action $NamespaceAMQTesting || true
-  if [ "$TransportType" == "AMQ" ]; then
-    deploy_consumer $action $NamespaceConsumerTesting $AMQTransportHost "" || true
-    deploy_producer $action $NamespaceProducerTesting $AMQTransportHost || true
-  else
-      deploy_consumer $action $NamespaceConsumerTesting $HTTPConsumerTransportHost $HTTPMockTransportHost || true
-      deploy_producer $action $NamespaceProducerTesting $HTTPMockTransportHost || true
-  fi
+  deploy_consumer $action $NamespaceConsumerTesting $HTTPConsumerTransportHost $HTTPMockTransportHost || true
+  deploy_producer $action $NamespaceProducerTesting $HTTPMockTransportHost || true
 fi
