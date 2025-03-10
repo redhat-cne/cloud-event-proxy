ac3ab313a82ec3a020269b37d9a3c415274fd099
David Rabkin <david@rabkin.co.il>
2025-03-10 21:46:27 +0200

    Manually align with main 2

--- hack/common/cne.sh
+++ hack/common/cne.sh
@@ -52,7 +52,8 @@ spec:
             - "--metrics-addr=127.0.0.1:9091"
             - "--store-path=/store"
             - "--transport-host=$transport_host"
-            - "--api-port=9045"
+            - "--api-version=2.0"
+            - "--api-port=9095"
           env:
             - name: NODE_NAME
               valueFrom:
