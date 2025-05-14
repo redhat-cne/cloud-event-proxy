68d96fba054cc20b660b7fc403de4ccdbbfbaeea
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-05-14 17:19:23 +0200

    [release-4.14] Fix consumer and e2e aws test (#512)
    * Enhance Consumer Application with Periodic Publisher Health Check
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * update dev dockerfiles
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * add PreferredDuringScheduling annotation to consumer
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    * Fix consumer image and e2e-aws test
    
    Fix event subscription in consumer example.
    Update consumer image and deployment.
    Remove amq from e2e test.
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    
    ---------
    
    Signed-off-by: Jack Ding <jackding@gmail.com>
    Co-authored-by: Jack Ding <jackding@gmail.com>

--- hack/common/common.sh
+++ hack/common/common.sh
@@ -4,9 +4,6 @@ set -e
 NODE_ROLE="${NODE_ROLE:-node-role.kubernetes.io/worker}"
 NamespaceProducerTesting="cloud-event-producer-testing"
 NamespaceConsumerTesting="cloud-event-consumer-testing"
-NamespaceAMQTesting="amq-router-testing"
-AMQTransportHost="amqp://amq-router.${NamespaceAMQTesting}.svc.cluster.local"
-TransportType="AMQP"
 HTTPMockTransportHost="mock-event-publisher-service.${NamespaceProducerTesting}.svc.cluster.local:9043"
 HTTPConsumerTransportHost="consumer-events-subscription-service.${NamespaceConsumerTesting}.svc.cluster.local:9043"
 
@@ -30,25 +27,7 @@ metadata:
     pod-security.kubernetes.io/warn: "privileged"
     #openshift.io/cluster-monitoring: "true"
 EOF
-create_amq_namespaces $action
 create_consumer_namespaces $action
-}
-create_amq_namespaces() {
-action=$1
-echo "$action namespace "
-cat <<EOF | oc $action -f -
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: $NamespaceAMQTesting
-  labels:
-    name: $NamespaceAMQTesting
-    security.openshift.io/scc.podSecurityLabelSync: "false"
-    pod-security.kubernetes.io/audit: "privileged"
-    pod-security.kubernetes.io/enforce: "privileged"
-    pod-security.kubernetes.io/warn: "privileged"
-    #openshift.io/cluster-monitoring: "true"
-EOF
 }
 
 create_consumer_namespaces(){
